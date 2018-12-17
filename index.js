c5f03605b41542697b25797b3457297282c7d964
rohitghai06 <43129818+rohitghai06@users.noreply.github.com>
2018-12-17 19:21:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import './main.scss';
+import datas from './data.json';
+//import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+// import bat from './bat.jpg';
+// import captain from './captain.jpg';
+// import iron from './iron.jpg';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 import Navbar from './Navbar';
 import 'semantic-ui-css/semantic.min.css';
@@ -10,88 +21,148 @@ import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 // A simple data API that will be used to get the data for our
 // components. On a real website, a more robust data fetching
 // solution would be more appropriate.
-const PlayerAPI = {
-  players: [
-    { number: 1, name: "Prashant Adiyodi", position: "G" },
-    { number: 2, name: "Rohit Ghai", position: "D" },
-    { number: 3, name: "Hemant Sahu", position: "D" }
-  ],
-  all: function() { return this.players},
-  get: function(id) {
-    const isPlayer = p => p.number === id
-    return this.players.find(isPlayer)
-  }
-}
+// const MemberAPI = {
+//     members: [
+//         { number: 1, name: "Rohit Ghai", img: captain, role: "Developer" },
+//         { number: 2, name: "Hemant Sahu", img: bat, role: "Developer" },
+//         { number: 3, name: "Prashant Adiyodi", img: iron, role: "Project Manager" }
+//     ],
+//     all: function () { return this.members },
+//     get: function (id) {
+//         const isMember = p => p.number === id
+//         return this.members.find(isMember)
+//     }
+// }
 
 // The FullRoster iterates over all of the players and creates
 // a link to their profile page.
-const FullRoster = () => (
-  <div>
-    <ul>
-      {
-        PlayerAPI.all().map(p => (
-          <li key={p.number}>
-            <Link to={`/roster/${p.number}`}>{p.name}</Link>
-          </li>
-        ))
-      }
-    </ul>
-  </div>
-)
-
-// The Player looks up the player using the number parsed from
-// the URL's pathname. If no player is found with the given
-// number, then a "player not found" message is displayed.
-// const Player = (props) => {
-//   const player = PlayerAPI.get(
-//     parseInt(props.match.params.number, 10)
-//   )
-//   if (!player) {
-//     return <div>Sorry, but the player was not found</div>
-//   }
-//   return (
+// const TeamList = () => (
 //     <div>
-//       <h1>{player.name} (#{player.number})</h1>
-//       <h2>Position: {player.position}</h2>
-//       <Link to='/roster'>Back</Link>
+
+//         <div class="ui horizontal list">
+//             {
+//                 MemberAPI.all().map(p => (
+//                     <div key={p.number} class="item">
+
+//                         <img class="ui mini circular image" src={p.img} />
+//                         <div class="content">
+//                             <div class="ui sub header"> <Link to={`/team/${p.number}`}>{p.name} </Link></div>
+//                             {p.role}
+//                         </div>
+//                     </div>
+//                 ))
+//             }
+//         </div>
 //     </div>
-//   )
+// )
+
+// The member looks up the member using the number parsed from
+// the URL's pathname. If no member is found with the given
+// number, then a "member not found" message is displayed.
+// const Member = (props) => {
+//     const member = MemberAPI.get(
+//         parseInt(props.match.params.number, 10)
+//     )
+//     if (!member) {
+//         return <div>Sorry, but the member was not found</div>
+//     }
+//     return (
+//         <div>
+//             <div className="ui card">
+//                 <div class="image">
+//                     <img src={member.img} alt="Logo" />
+//                 </div>
+//                 <div className="content">
+//                     <a className="header">{member.name}</a>
+//                 </div></div>
+//             <h4>Position:<b>{member.role}</b></h4>
+//             <Link to='/team'>Back</Link>
+//         </div>
+//     )
 // }
 
-const Chota = () => (
-  <div>
-  I am internal route
-   <Link to='/roster'>Back</Link>
-  </div>
-)
 
-// The Roster component matches one of two different routes
+// The team component matches one of two different routes
 // depending on the full pathname
-const Roster = () => (
-  <Switch>
-    <Route exact path='/roster' component={FullRoster}/>
-    <Route path='/roster/:number' component={Chota}/>
-  </Switch>
-)
+// const Team = () => (
+//     <Switch>
+//         <Route exact path='/team' component={TeamList} />
+//         <Route path='/team/:number' component={Member} />
+//     </Switch>
+// )
 
-const AboutUs = () => (
-  <div>
-    <h1>inside About Us</h1>
-  </div>
-)
+// const AboutUs = () => (
+//     <div>
+//         <h1>inside About Us</h1>
+      
+//         <video controls="controls"
+//             class="video-stream"
+//             x-webkit-airplay="allow"
+//             data-youtube-id="N9oxmRT2YWw"
+//             src="http://www.youtube.com/watch?v=OmxT8a9RWbE"
+//         ></video>
 
-const ContactUs = () => (
-  <div>
-    <h1>inside Contact Us</h1>
-  </div>
-)
+//     </div>
+// )
 
+// const ContactUs = () => (
+//     <div>
+//         <div className="ui card">
+//             <div className="content">
+//                 <a className="header" href="#link">Steve Jobes</a>
+//             </div></div>
+//     </div>
+// )
 
-const Home = () => (
-  <div>
-    <h1>Welcome to the Tornadoes Website!</h1>
-  </div>
-)
+var activateLasers = () => {
+    console.log("called")
+    return(
+    <div>
+        <div className="ui card">
+            <div className="content">
+                <a className="header" href="#link">Steve Jobes</a>
+            </div></div>
+    </div>)
+}
+
+const Home = () => {
+    console.log(datas)
+    return(
+    <div>
+        <h1>List Of Items </h1>
+        
+
+            <table className="ui inverted table">
+                <thead>
+                    <tr><th>Name</th>
+                        <th>Price</th>
+                        <th>Weight</th>
+                        <th>Choose Items</th>
+                    </tr></thead>
+                <tbody>
+                    {
+                        datas.map(data => (
+
+                            <tr key={data.Name}>
+                                <td data-label="Name">{data.Name}</td>
+                                <td data-label="Price">{data.Price}</td>
+                                <td data-label="Weight">{data.Weight}</td>
+                                <td>
+                                    <div className="ui checkbox">
+                                        <input type="checkbox" name="example"/>
+                                            <label></label>
+                                        </div>
+                                </td>
+                            </tr>
+                        ))
+                    }
+
+                </tbody>
+            </table>
+            <button class="ui inverted secondary button">Place Order</button>
+            <br/> <br/> <br/> <br/>
+    </div>)
+}
 
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
@@ -99,35 +170,36 @@ const Home = () => (
 // with /roster or /schedule. The / route will only match
 // when the pathname is exactly the string "/"
 const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/roster' component={Roster}/>
-      <Route path='/about' component={AboutUs}/>
-      <Route path='/contact' component={ContactUs}/>
-    </Switch>
-  </main>
+    <main>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            {/* <Route path='/team' component={Team} />
+            <Route path='/about' component={AboutUs} />
+            <Route path='/contact' component={ContactUs} /> */}
+        </Switch>
+    </main>
 )
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
-  <header>
-  <Navbar/>
-  </header>
+    <header>
+        <Navbar />
+    </header>
 )
 
 const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
+    <div>
+        <Header />
+        <Main />
+    </div>
 )
 
 // This demo uses a HashRouter instead of BrowserRouter
 // because there is no server to match URLs
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'))
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root'));
+registerServiceWorker();
